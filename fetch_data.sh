33862fc351ae7e759388b6b9d91312261be4a60a
Jangsea Park <jangsea.park@gmail.com>
2016-12-07 14:58:56 +0900

    Save csv files to www root

--- fetch_data.sh
+++ fetch_data.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 TODAY="https://docs.google.com/spreadsheets/u/1/d/1Z4zDkFS0S6KSjAybBM2e7vFI8muhyJTSkQpfJvmqxXs/export?format=csv&id=1Z4zDkFS0S6KSjAybBM2e7vFI8muhyJTSkQpfJvmqxXs&gid=0"
 SEVENDAYS="https://docs.google.com/spreadsheets/u/1/d/1Z4zDkFS0S6KSjAybBM2e7vFI8muhyJTSkQpfJvmqxXs/export?format=csv&id=1Z4zDkFS0S6KSjAybBM2e7vFI8muhyJTSkQpfJvmqxXs&gid=1232289472"
-sudo wget "${TODAY}" -Otoday.csv -q
-sudo wget "${SEVENDAYS}" -Osevendays.csv -q
+SCRIPTDIR=`dirname ${BASH_SOURCE}`
+sudo wget "${TODAY}" -O${SCRIPTDIR}/www/today.csv -q
+sudo wget "${SEVENDAYS}" -O${SCRIPTDIR}/www/sevendays.csv -q
