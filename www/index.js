fd82fc59ea21aa9534075d9d6b5004e5dececc71
Jangsea Park <jangsea.park@gmail.com>
2016-12-09 12:02:35 +0900

    Show map

--- www/index.js
+++ www/index.js
@@ -1,9 +1,41 @@
+var DEFAULT_LONLAT = [126.9746921, 37.5728438];
+
+var map;
+
 function main() {
     var igniteEl = document.getElementById('ignite');
-    igniteEl.addEventListener('click', function(e) {
+    igniteEl.addEventListener('click', function (e) {
         e.preventDefault();
         ga('send', 'event', 'data', 'click', 'ignite');
     });
+
+    map = new ol.Map({
+        target: 'map',
+        layers: [
+            new ol.layer.Tile({
+                source: new ol.source.OSM()
+            })
+        ],
+        view: new ol.View({
+            center: ol.proj.fromLonLat(DEFAULT_LONLAT),
+            zoom: 16
+        })
+    });
+
+    getCurrentPosition(function (lonlat) {
+        map.getView().setCenter(ol.proj.fromLonLat(lonlat));
+    });
+}
+
+
+function getCurrentPosition(callback) {
+    if ('geolocation' in navigator) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            callback([position.coords.longitude, position.coords.latitude]);
+        });
+    } else {
+        callback([126.9746921, 37.5728438]);
+    }
 }
 
 
