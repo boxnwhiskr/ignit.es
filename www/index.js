5bc85767cfec329b955a9b3979d7aeeb549e79b7
Jangsea Park <jangsea.park@gmail.com>
2016-12-09 13:36:51 +0900

    Use "dark matter" tile

--- www/index.js
+++ www/index.js
@@ -20,7 +20,7 @@ function main() {
         event.feature.set('weight', magnitude);
     });
     var tile = new ol.layer.Tile({
-        source: new ol.source.OSM()
+        source: new ol.source.OSM({'url': 'http://{a-c}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png'})
     });
 
     map = new ol.Map({
