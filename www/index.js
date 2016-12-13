2c6ec350b57ff45558d39828f14196c9a8b6be51
akngs <jania902@gmail.com>
2016-12-13 10:18:50 +0900

    Show local data in realtime

--- www/index.js
+++ www/index.js
@@ -1,162 +1,178 @@
 var DEFAULT_LONLAT = [126.9746921, 37.5728438];
 
 var map;
+var heatmap;
 var lonlat = DEFAULT_LONLAT;
 var ignitedUntil = 0;
 
 function main() {
-    var heatmap = new ol.layer.Heatmap({
-        source: new ol.source.Vector({
-            url: 'today.kml',
-            format: new ol.format.KML({
-                extractStyles: false
-            })
-        }),
-        blur: 20,
-        radius: 10
-    });
-    heatmap.getSource().on('addfeature', function (event) {
-        var name = event.feature.get('name');
-        var magnitude = parseFloat(name);
-        event.feature.set('weight', magnitude);
-    });
-    var tile = new ol.layer.Tile({
-        source: new ol.source.OSM({
-            'url': '//cartodb-basemaps-{a-c}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png'
-        })
-    });
+  heatmap = new ol.layer.Heatmap({
+    source: new ol.source.Vector({
+      url: 'today.kml',
+      format: new ol.format.KML({
+        extractStyles: false
+      })
+    }),
+    blur: 20,
+    radius: 10
+  });
+  heatmap.getSource().on('addfeature', function (event) {
+    var name = event.feature.get('name');
+    var magnitude = parseFloat(name);
+    event.feature.set('weight', magnitude);
+  });
+  var tile = new ol.layer.Tile({
+    source: new ol.source.OSM({
+      'url': '//cartodb-basemaps-{a-c}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png'
+    })
+  });
 
-    map = new ol.Map({
-        target: 'map',
-        layers: [tile, heatmap],
-        view: new ol.View({
-            center: ol.proj.fromLonLat(DEFAULT_LONLAT),
-            zoom: 12,
-            minZoom: 3,
-            maxZoom: 12
-        }),
-        interactions: ol.interaction.defaults({
-            altShiftDragRotate: false,
-            pinchRotate: false
-        })
+  map = new ol.Map({
+    target: 'map',
+    layers: [tile, heatmap],
+    view: new ol.View({
+      center: ol.proj.fromLonLat(DEFAULT_LONLAT),
+      zoom: 12,
+      minZoom: 3,
+      maxZoom: 12
+    }),
+    interactions: ol.interaction.defaults({
+      altShiftDragRotate: false,
+      pinchRotate: false
+    })
+  });
+
+  getCurrentPosition(function (newLonlat) {
+    map.getView().setCenter(ol.proj.fromLonLat(newLonlat));
+    lonlat = newLonlat;
+
+    var igniteEl = document.getElementById('ignite');
+    igniteEl.addEventListener('click', function (e) {
+      e.preventDefault();
+      onIgniteClick();
     });
+  });
 
-    getCurrentPosition(function (newLonlat) {
-        map.getView().setCenter(ol.proj.fromLonLat(newLonlat));
-        lonlat = newLonlat;
+  var data = JSON.parse(getCookie('ignited') || '{}');
+  if (data.expDate) {
+    document.getElementById('ignite').setAttribute('class', 'ignited');
+    ignitedUntil = data.expDate;
+    ignite(data.lonlat, 1);
+  }
 
-        var igniteEl = document.getElementById('ignite');
-        igniteEl.addEventListener('click', function (e) {
-            e.preventDefault();
-            onIgniteClick();
-        });
-    });
-
-    var expInSec = getCookie('ignited');
-    if (expInSec !== null) {
-        document.getElementById('ignite').setAttribute('class', 'ignited');
-        ignitedUntil = +expInSec;
-    }
-
-    update();
+  update();
 }
 
+function ignite(lonlat, count) {
+  var point = new ol.geom.Point(ol.proj.fromLonLat(lonlat));
+  var feature = new ol.Feature({
+    'name': '' + count,
+    'geometry': point
+  });
+  heatmap.getSource().addFeature(feature);
+}
 
 function getBinnedLonlat(lonlat) {
-    var bin = 50;
-    var center = (1 / bin / 2);
-    return [
-        ((lonlat[0] * bin) | 0) / bin + center,
-        ((lonlat[1] * bin) | 0) / bin + center
-    ];
+  var bin = 50;
+  var center = (1 / bin / 2);
+  return [
+    ((lonlat[0] * bin) | 0) / bin + center,
+    ((lonlat[1] * bin) | 0) / bin + center
+  ];
 }
 
 
 function getCurrentPosition(callback) {
-    if ('geolocation' in navigator) {
-        navigator.geolocation.getCurrentPosition(function (position) {
-            callback([position.coords.longitude, position.coords.latitude]);
-        }, function () {
-            document.body.setAttribute('class', 'nogeo');
-            callback(DEFAULT_LONLAT);
-        });
-    } else {
-        document.body.setAttribute('class', 'nogeo');
-        callback(DEFAULT_LONLAT);
-    }
+  if ('geolocation' in navigator) {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      callback([position.coords.longitude, position.coords.latitude]);
+    }, function () {
+      document.body.setAttribute('class', 'nogeo');
+      callback(DEFAULT_LONLAT);
+    });
+  } else {
+    document.body.setAttribute('class', 'nogeo');
+    callback(DEFAULT_LONLAT);
+  }
 }
 
 function onIgniteClick() {
-    var ignited = getCookie('ignited') !== null;
-    if (ignited) return;
+  var ignited = getCookie('ignited') !== null;
+  if (ignited) return;
 
-    var binnedLonlat = getBinnedLonlat(lonlat);
-    ga('set', 'dimension1', binnedLonlat[0]);
-    ga('set', 'dimension2', binnedLonlat[1]);
-    ga('send', 'event', 'data', 'click', 'ignite');
-    new Image().src = 'ignited';
+  var binnedLonlat = getBinnedLonlat(lonlat);
+  ga('set', 'dimension1', binnedLonlat[0]);
+  ga('set', 'dimension2', binnedLonlat[1]);
+  ga('send', 'event', 'data', 'click', 'ignite');
+  new Image().src = 'ignited';
 
-    var expInSec = 60 * 60;
-    var expDate = new Date(Date.now() + expInSec * 1000);
+  var expInSec = 60 * 60;
+  var expDate = new Date(Date.now() + expInSec * 1000);
 
-    setCookie('ignited', expDate.getTime(), expInSec);
-    document.getElementById('ignite').setAttribute('class', 'ignited');
-    ignitedUntil = expDate.getTime();
+  var data = {
+    'lonlat': binnedLonlat,
+    'expDate': expDate.getTime()
+  };
+  setCookie('ignited', JSON.stringify(data), expInSec);
+  document.getElementById('ignite').setAttribute('class', 'ignited');
+  ignitedUntil = expDate.getTime();
+
+  ignite(binnedLonlat, 1);
 }
 
 
 function getCookie(name) {
-    var pairs = document.cookie.split('; ');
-    for (var i = 0; i < pairs.length; i++) {
-        var kv = pairs[i].split('=');
-        if (kv[0] === name) return decodeURIComponent(kv[1]);
-    }
-    return null;
+  var pairs = document.cookie.split('; ');
+  for (var i = 0; i < pairs.length; i++) {
+    var kv = pairs[i].split('=');
+    if (kv[0] === name) return decodeURIComponent(kv[1]);
+  }
+  return null;
 }
 
 
 function setCookie(name, value, expiresInSec, path, domain, secure) {
-    var cookieStr = name + "=" + encodeURIComponent(value) + "; ";
+  var cookieStr = name + "=" + encodeURIComponent(value) + "; ";
 
-    if (expiresInSec) {
-        var expDate = new Date(Date.now() + expiresInSec * 1000);
-        cookieStr += "expires=" + expDate.toUTCString() + "; ";
-    }
-    if (path) {
-        cookieStr += "path=" + path + "; ";
-    }
-    if (domain) {
-        cookieStr += "domain=" + domain + "; ";
-    }
-    if (secure) {
-        cookieStr += "secure; ";
-    }
-    document.cookie = cookieStr;
+  if (expiresInSec) {
+    var expDate = new Date(Date.now() + expiresInSec * 1000);
+    cookieStr += "expires=" + expDate.toUTCString() + "; ";
+  }
+  if (path) {
+    cookieStr += "path=" + path + "; ";
+  }
+  if (domain) {
+    cookieStr += "domain=" + domain + "; ";
+  }
+  if (secure) {
+    cookieStr += "secure; ";
+  }
+  document.cookie = cookieStr;
 }
 
 
 function update() {
-    var minute = 60;
-    var second = 0;
-    var now = Date.now();
-    if (now > ignitedUntil) {
-        document.querySelector('#ignite').setAttribute('class', '');
-    } else {
-        var totalSec = ((ignitedUntil - now) / 1000) | 0;
-        second = totalSec % 60;
-        minute = (totalSec / 60) | 0;
-    }
+  var minute = 60;
+  var second = 0;
+  var now = Date.now();
+  if (now > ignitedUntil) {
+    document.querySelector('#ignite').setAttribute('class', '');
+  } else {
+    var totalSec = ((ignitedUntil - now) / 1000) | 0;
+    second = totalSec % 60;
+    minute = (totalSec / 60) | 0;
+  }
 
-    var minuteStr = '0' + minute;
-    var secondStr = '0' + second;
+  var minuteStr = '0' + minute;
+  var secondStr = '0' + second;
 
-    var untilEl = document.querySelector('#ignite .until');
-    untilEl.innerHTML = (
-        minuteStr.substr(minuteStr.length - 2) + ':' +
-        secondStr.substr(secondStr.length - 2)
-    );
+  var untilEl = document.querySelector('#ignite .until');
+  untilEl.innerHTML = (
+    minuteStr.substr(minuteStr.length - 2) + ':' +
+    secondStr.substr(secondStr.length - 2)
+  );
 
-    window.setTimeout(update, 100);
+  window.setTimeout(update, 100);
 }
 
 
